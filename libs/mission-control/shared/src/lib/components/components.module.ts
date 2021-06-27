3bdcebfb2727c5140e7d3f43c8abaa2acad19a69
Kyle Martin Harman <kylemartinharman@gmail.com>
2021-06-27 11:58:40 +1000

    updated ui

--- libs/mission-control/shared/src/lib/components/components.module.ts
+++ libs/mission-control/shared/src/lib/components/components.module.ts
@@ -3,13 +3,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgMaterialModule } from '@harman/ng-material';
 import { NgSharedModule } from '@harman/ng-shared';
+import { TooltipModule } from 'ng2-tooltip-directive';
 import { ThemeService } from '../services/theme.service';
 import { AppShellComponent } from './app-shell/app-shell.component';
 import { SpacesBarComponent } from './spaces-bar/spaces-bar.component';
 import { UserSettingsMenuComponent } from './user-settings-menu/user-settings-menu.component';
 
 @NgModule({
-  imports: [CommonModule, NgSharedModule, NgMaterialModule, RouterModule],
+  imports: [
+    CommonModule,
+    NgSharedModule,
+    NgMaterialModule,
+    RouterModule,
+    TooltipModule,
+  ],
   declarations: [
     SpacesBarComponent,
     AppShellComponent,
